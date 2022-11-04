e2a1e020f2118afc2e7b9d90f4789bb2c7e4e5fb
zkdlu <zkdlu159951@gmail.com>
2022-11-04 15:56:37 +0900

    init

---
+++ src/main/java/com/zkdlu/reactive/ReacitveApplication.java
@@ -0,0 +1,11 @@
+package com.zkdlu.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReacitveApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReacitveApplication.class);
+    }
+}
